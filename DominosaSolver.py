9031f1c5b74a42f7247fc795a0df6d326652edb0
Madison Everett <madison.everett@pop,belmont.edu>
2018-11-26 23:18:16 -0600

    Progress

--- DominosaSolver.py
+++ DominosaSolver.py
@@ -280,7 +280,17 @@ def solveReally():
           3: [1, 2, 4, 5, 6],
           4: [1, 2, 3, 5, 6],
           5: [1, 2, 3, 4, 6],
-          6: [1, 2, 3, 4, 5], }
+          6: [1, 2, 3, 4, 5],
+          7: Really,
+          8: Really,
+          9: Really,
+          10: Really,
+          11: Really,
+          12: Really,
+          13: Really,
+          14: Really,
+          15: Really,
+          }
 
     Dominosa1 = CSP(v2, d2, n2, different_values_constraint)
 
@@ -319,12 +329,26 @@ def solveSuper():
           3: [1, 2, 4, 5, 6],
           4: [1, 2, 3, 5, 6],
           5: [1, 2, 3, 4, 6],
-          6: [1, 2, 3, 4, 5], }
+          6: [1, 2, 3, 4, 5],
+          7: Super,
+          8: Super,
+          9: Super,
+          10: Super,
+          11: Super,
+          12: Super,
+          13: Super,
+          14: Super,
+          15: Super,
+
+
+
+
+
+          }
 
     Dominosa1 = CSP(v2, d2, n2, different_values_constraint)
 
-    print(backtracking_search(Dominosa1, select_unassigned_variable=mrv, order_domain_values=lcv,
-                              inference=forward_checking))
+    backtracking_search(Dominosa1, select_unassigned_variable=mrv, order_domain_values=lcv,inference=None)
 
 
 
@@ -332,7 +356,11 @@ def solveGenius():
     print('Absolutely Genius')
     print(prettyprintboards(AbsolutelyGenius))
 
-    Genius = []
+    Genius = ['3|4','1|4','1|2','2|4','2|4','0|1','2|2','1|3','1|2','0|3','0|2','0|2','0|0','2|3','1|5','0|2','2|5','1|3','2|5','0|5','2|5','2|2','0|3','4|4','1|1',
+              '2|3','0|0','1|4','3|6','1|1','0|2','0|1','0|4','1|3','2|3','0|4','0|5','2|3','3|5','0|3','2|6','1|5','5|6','0|3','2|5','1|4','4|5','3|5','1|4','1|5',
+              '2|6','1|5', '4|5', '5|5', '6|6', '2|5', '4|6', '4|6', '1|5', '4|5', '4|6', '0|6', '4|5', '5|6', '4|4','0|6', '0|0', '1|5', '4|5', '1|6', '1|6', '3|5',
+              '4|5', '3|6', '1|6', '1|6', '6|6', '3|5', '2|4', '2|6', '3|6', '4|4', '2|6', '0|6', '1|4', '1|5', '0|6','2|6', '4|6', '3|3', '1|6', '3|6', '3|3', '0|6',
+              '2|4', '1|3', '2|4', '1|3',]
 
     d2 = {1: Genius,
           2: Genius,
@@ -366,17 +394,39 @@ def solveGenius():
 
     v2 = d2.keys()
 
-    n2 = {1: [2, 3, 4, 5, 6],
-          2: [1, 3, 4, 5, 6],
+    n2 = {1: [2, 3, 4, 5, 6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28],
+          2: [1, 3, 4, 5, 6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28],
           3: [1, 2, 4, 5, 6],
           4: [1, 2, 3, 5, 6],
           5: [1, 2, 3, 4, 6],
-          6: [1, 2, 3, 4, 5], }
+          6: [1, 2, 3, 4, 5],
+          7: Genius,
+          8: Genius,
+          9: Genius,
+          10: Genius,
+          11: Genius,
+          12: Genius,
+          13: Genius,
+          14: Genius,
+          15: Genius,
+          16: Genius,
+          17: Genius,
+          18: Genius,
+          19: Genius,
+          20: Genius,
+          21: Genius,
+          22: Genius,
+          23: Genius,
+          24: Genius,
+          25: Genius,
+          26: Genius,
+          27: Genius,
+          28: Genius,
+          }
 
     Dominosa1 = CSP(v2, d2, n2, different_values_constraint)
 
-    print(backtracking_search(Dominosa1, select_unassigned_variable=mrv, order_domain_values=lcv,
-                              inference=forward_checking))
+    backtracking_search(Dominosa1, select_unassigned_variable=mrv, order_domain_values=lcv,inference=forward_checking)
 
 
 def solveboard(array):
